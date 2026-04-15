83a434a5716ab16b7b1f8f1669b04d81fa719ce9
Eshrath027 <shaikeshrath027@gmail.com>
2026-04-15 20:59:16 +0530

    changes

--- utils/prepare-aspm-scanners.sh
+++ utils/prepare-aspm-scanners.sh
@@ -134,7 +134,7 @@ echo "✅ OpenJDK ${JDK_VERSION} + ZAP ${ZAP_VERSION} packaged into '$DAST_TAR'"
 
 ### CodeAssure -> codeassure.tar.gz
 echo "=== [7/7] Building CodeAssure ==="
-CODEASSURE_REPO=" https://github.com/Eshrath027/codeassure-cli.git"
+CODEASSURE_REPO="https://github.com/Eshrath027/codeassure-cli.git"
 CODEASSURE_VERSION="v0.1.4"
 CODEASSURE_FOLDER="codeassure"
 CODEASSURE_TAR="codeassure.tar.gz"
